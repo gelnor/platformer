65253e9af04194b17f241aef6951e62f2d31cff0
Ivan Frantsuzov <ifrantsuzov@gmail.com>
2022-09-16 17:44:13 +0300

    commit

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,18 @@
 extends KinematicBody2D
 
+export(int) var JUMP_FORCE = -130
+export(int) var JUMP_RELEASE_FORCE = -70
+export(int) var MAX_SPEED = 50
+export(int) var ACCELERATION = 10
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 4
+export(int) var FALL_GRAVITY = 4
+export(int) var MAX_GRAVITY = 300
+
 
 var velocity = Vector2.ZERO
+
+onready var animatedSprite = $AnimatedSprite
 
 func _physics_process(delta):
 	var input  = Vector2.ZERO
@@ -11,21 +22,41 @@ func _physics_process(delta):
 	
 	if input.x == 0:
 		apply_friction()
+		animatedSprite.animation = 'Idle';
 	else:
 		apply_acceleration(input.x)
+		animatedSprite.animation = 'Run';
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		elif input.x < 0:
+			animatedSprite.flip_h = false
 		
-	if Input.is_action_pressed("ui_jump"):
-		velocity.y =  -200		
-		
-	velocity = move_and_slide(velocity)
-	pass
+	if is_on_floor():
+		if Input.is_action_pressed("ui_jump"):
+			velocity.y =  JUMP_FORCE
+			
+	else:
+		animatedSprite.animation = 'Jump';
+		if Input.is_action_just_released("ui_jump") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
+		if velocity.y > 0:
+			velocity.y += FALL_GRAVITY
+			
+	var was_in_air = not is_on_floor()
+	velocity = move_and_slide(velocity,Vector2.UP)
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		animatedSprite.animation = 'Run'
+		animatedSprite.frame = 1
+	
 
 func apply_gravity():
-	velocity.y += 10
+	velocity.y += GRAVITY
+	velocity.y = min(MAX_GRAVITY,velocity.y)
 
 func apply_friction():
-	pass
+	velocity.x =  move_toward(velocity.x, 0, FRICTION)
 	
 func apply_acceleration(amount):
-	velocity.x =  move_toward(velocity.x, 50 * amount, 20)
+	velocity.x =  move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
 	
